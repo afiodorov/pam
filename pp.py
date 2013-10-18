66ad3010e36e09d676280c939667bf05cfe06dc9
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2013-10-18 17:36:36 +0100

    2 graphs working

--- pp.py
+++ pp.py
@@ -6,10 +6,13 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
 time_step = 1
-weib_par = 20
-num_of_points_per_frame = 10
+num_of_points_per_frame = 1
 initial_size = 300
+weib_par = 1.5
+pareto_par = 1
 
+#gen_potential = lambda: np.random.pareto(pareto_par)
+gen_potential = lambda: np.random.weibull(weib_par)
 
 def a(time):
     return m.log(time) ** (1 / weib_par)
@@ -27,33 +30,63 @@ def psi_rescale(x, i, time):
     return (x - a(time) - i * m.log(m.log(time)) / (time * weib_par)) / d(time)
 
 
-initial_time = 1.2
+initial_time = initial_size
 #initial_time = initial_size * time_step // (num_of_points_per_frame + 1)
 
 
-def data_gen(framenumber, potential):
+def data_gen(framenumber, pospotential, negpotential, soln):
     for i in range(num_of_points_per_frame):
-        potential.append(np.random.weibull(weib_par))
-    time = framenumber * time_step + initial_time
-    #points = [rescale(x, time) for x in potential]
-    points = [psi_rescale(x, i, time) for x, i in zip(potential,
-                                                               range(len(potential)))]
+        pospotential.append(gen_potential())
+        negpotential.append(gen_potential())
+
+    time = 2 * framenumber * time_step + initial_time
+    points = [psi_rescale(x, i, time) for x, i in zip(pospotential,
+                                                      range(len(pospotential)))]
+    negpoints = [psi_rescale(x, i, time) for x, i in zip(negpotential,
+                                                         range(len(negpotential)))]
     #ax.relim()
     #ax.autoscale_view()
-    ax.set_xlim((1, len(points)))
-    #ax.set_xlim((1, 100))
-    ax.set_ylim((-10, 10))
-    plot.set_data(range(len(points)), points)
-    return plot,
+    negpoints.reverse()
+    ax_pp.set_xlim((-len(points), len(points)))
+    ax_pp.set_ylim((max(negpoints + points) - 8, max(negpoints + points) +
+                    0.1))
+    plot_pp.set_data(range(-len(points), len(points)), negpoints + points)
+
+    for i in range(num_of_points_per_frame):
+        soln.append(0)
+        oldsoln = soln[:]
+        time_step_pam = time_step // num_of_points_per_frame
+        for n in range(1, len(pospotential) - 1):
+            soln[n] = oldsoln[n] + time_step_pam * (
+                        oldsoln[n] * (pospotential[n] - 2)
+                        + oldsoln[n - 1] + oldsoln[n + 1])
+
+        norm = max(soln)
+        soln = [x / norm for x in soln]
+    ax_pam.set_xlim((-len(soln), len(soln)))
+    ax_pam.set_ylim((0,1))
+    plot_pam.set_data(range(len(soln)), soln)
+
+    return plot_pp, plot_pam
 
 
-potential = np.random.weibull(weib_par, initial_size).tolist()
-points = [rescale(x, initial_time) for x in potential]
+
+pospotential, negpotential = [], []
+for n in range(initial_size):
+    pospotential.append(gen_potential())
+    negpotential.append(gen_potential())
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-plot, = plt.plot(range(initial_size), points, 'ro')
-ax.cla()
-pam_ani = animation.FuncAnimation(fig, data_gen, fargs=([potential]),
+ax_pp = fig.add_subplot(121)
+ax_pam = fig.add_subplot(122)
+plot_pp, = ax_pp.plot([1], [1], 'ro', animated=True)
+plot_pam, = ax_pam.plot([0], [0], animated=True)
+ax_pp.cla()
+
+soln = [0] * (initial_size + num_of_points_per_frame)
+soln[0] = 1
+
+pam_ani = animation.FuncAnimation(fig, data_gen, fargs=(pospotential,
+                                                        negpotential, soln),
                                   interval=1, blit=True)
 plt.show()
