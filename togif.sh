ecbd2a7d0d96b580338ca2a6bb214b390136b0da
Tom Artiom Fiodorov <art.fiodorov@gmail.com>
2013-10-24 08:48:42 +0100

    now togif takes arb # of arguments

--- togif.sh
+++ togif.sh
@@ -2,12 +2,14 @@
 if [ ! -d "./animations" ]; then
 	mkdir tmp
 fi
-
-filename=$(basename "$1")
-filename="${filename%.*}"
-giffile="${filename}.gif"
-#ffmpeg -ss 00:00:00.000 -i $1 -pix_fmt rgb24 -r 10 -s 1024x768 -t 00:00:20.000 $filename.temp.gif
-ffmpeg -ss 00:00:00.000 -i $1 -pix_fmt rgb24 -r 10 -s 1024x768 $filename.temp.gif
-convert -layers Optimize $filename.temp.gif $giffile 
-mv $giffile "./animations/${giffile}"
-rm $filename.temp.gif
+for ARG in $*
+do
+	filename=$(basename "$ARG")
+	filename="${filename%.*}"
+	giffile="${filename}.gif"
+	#ffmpeg -ss 00:00:00.000 -i $1 -pix_fmt rgb24 -r 10 -s 1024x768 -t 00:00:20.000 $filename.temp.gif
+	ffmpeg -ss 00:00:00.000 -i $ARG -pix_fmt rgb24 -r 10 -s 1024x768 $filename.temp.gif
+	convert -layers Optimize $filename.temp.gif $giffile 
+	mv $giffile "./animations/${giffile}"
+	rm $filename.temp.gif
+done
